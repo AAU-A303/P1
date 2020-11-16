43c73fac965c95f0c67a7146cf647872f2dcc447
Jamie Hammer <jamie-hammer@hotmail.com>
2020-11-16 09:48:55 +0100

    init commit
    File setup

---
+++ Programme/energy.h
@@ -0,0 +1,16 @@
+/*
+ * Programmer: Jamie Hammer     Date completed: 16th of November, 2020
+ * Study Group: A303            E-mail: jhamme20@student.aau.dk
+ * Supervisor: Kamal Shahid     Class: P1, Computer Science - Fall Semester
+ *
+ * Description: Here goes a description of this file.
+ */
+
+#ifndef ENERGY_H
+#define ENERGY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
---
+++ Programme/main.c
@@ -0,0 +1,16 @@
+/*
+ * Programmer: Jamie Hammer     Date completed: 16th of November, 2020
+ * Study Group: A303            E-mail: jhamme20@student.aau.dk
+ * Supervisor: Kamal Shahid     Class: P1, Computer Science - Fall Semester
+ *
+ * Description: Here goes a description of this file.
+ */
+
+#include "energy.h"
+
+int main(void)
+{
+    /* program starts here */
+
+    return EXIT_SUCCESS;
+}
