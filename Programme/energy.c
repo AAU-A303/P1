912191ef005b4ae06c4df8b011178089b246d521
Jamie Hammer <jamie-hammer@hotmail.com>
2020-11-17 17:25:11 +0100

    Updated headers
    Updated the remaining headers to follow the new convention. This is the way!

--- Programme/energy.c
+++ Programme/energy.c
@@ -1,17 +1,21 @@
 /*
+ ===============================================================================
  * File: energy.c               Date completed: 16th of November, 2020
- * Programmers: Anders Geert    E-mail: ageert20@student.aau.dk
- *              Christoffer Jønsson     cjanss20@student.aau.dk
- *              Dennis Larsen           dbla19@student.aau.dk
- *              Jamie Hammer            jhamme20@student.aau.dk
- *              Loke Walsted            lwalst20@student.aau.dk
- *              Mads Lundfold           mlundf20@student.aau.dk
- *              Master Piyaboot         mprase20@student.aau.dk
+ ===============================================================================
+ * Programmers: Anders G.       E-mail: ageert20@student.aau.dk
+ *              Christoffer J.          cjanss20@student.aau.dk
+ *              Dennis L.               dbla19@student.aau.dk
+ *              Jamie H.                jhamme20@student.aau.dk
+ *              Loke W.                 lwalst20@student.aau.dk
+ *              Mads L.                 mlundf20@student.aau.dk
+ *              Master P.               mprase20@student.aau.dk
+ ===============================================================================
  * Study Group: A303
  * Supervisor: Kamal Shahid     Class: P1, Computer Science - Fall Semester
- *
+ ===============================================================================
  * Description: This is responsible for managing most of our logic of this
  *              programme.
+ ===============================================================================
  */
 
 #include "energy.h"
