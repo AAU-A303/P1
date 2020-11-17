341deff1b1fdd8a0a56d0989d9d6461660e9e48c
Jamie Hammer <jamie-hammer@hotmail.com>
2020-11-18 00:32:05 +0100

    Yet another Header quest
    Last update for tonight

--- Programme/energy.h
+++ Programme/energy.h
@@ -1,17 +1,17 @@
 /*
  ===============================================================================
- * File: energy.h               Date completed: 16th of November, 2020
+ * File: energy.h                   Date completed: 16th of November, 2020
  ===============================================================================
- * Programmers: Anders G.       E-mail: ageert20@student.aau.dk
- *              Christoffer J.          cjanss20@student.aau.dk
- *              Dennis L.               dbla19@student.aau.dk
- *              Jamie H.                jhamme20@student.aau.dk
- *              Loke W.                 lwalst20@student.aau.dk
- *              Mads L.                 mlundf20@student.aau.dk
- *              Master P.               mprase20@student.aau.dk
+ * Programmers: Anders G.           E-mail: ageert20@student.aau.dk
+ *              Christoffer J.              cjanss20@student.aau.dk
+ *              Dennis L.                   dbla19@student.aau.dk
+ *              Jamie H.                    jhamme20@student.aau.dk
+ *              Loke W.                     lwalst20@student.aau.dk
+ *              Mads L.                     mlundf20@student.aau.dk
+ *              Master P.                   mprase20@student.aau.dk
  ===============================================================================
  * Study Group: A303
- * Supervisor: Kamal Shahid     Class: P1, Computer Science - Fall Semester
+ * Supervisor: Kamal Shahid         Class: P1, Computer Science - Fall Semester
  ===============================================================================
  * Description: This is our main header file. This holds all of our prototypes,
  *              symbolic constants and included libraries.
