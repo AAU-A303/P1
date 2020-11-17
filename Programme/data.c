e47a3967020d903f09a20d7e50a4a057465b7f84
LowkeyCoding <loke0046@gmail.com>
2020-11-17 10:19:33 +0100

    Basic strutcs for storing Date and Energy_price

--- Programme/data.c
+++ Programme/data.c
@@ -8,6 +8,17 @@
 
 #include "energy.h"
 
+struct Date {
+    int day;
+    int month;
+    int year;
+}
+
+struct Energy_Price {
+    struct Date date;
+    float[24] prices;
+}
+
 int main(void)
 {
     /* program starts here */
