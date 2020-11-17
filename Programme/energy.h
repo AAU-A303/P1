8893476f7554317e787351017a7d29ca0272c8f0
Jamie Hammer <jamie-hammer@hotmail.com>
2020-11-17 10:20:03 +0100

    Updated the header file
    Added all of our names

--- Programme/energy.h
+++ Programme/energy.h
@@ -1,9 +1,16 @@
 /*
- * Programmer: Jamie Hammer     Date completed: 16th of November, 2020
- * Study Group: A303            E-mail: jhamme20@student.aau.dk
+ * File: main.c                 Date completed: 16th of November, 2020
+ * Programmers: Anders Geert    E-mail: ageert20@student.aau.dk
+ *              Christoffer Jønsson     cjanss20@student.aau.dk
+ *              Dennis Larsen           dbla19@student.aau.dk
+ *              Jamie Hammer            jhamme20@student.aau.dk
+ *              Loke Walsted            lwalst20@student.aau.dk
+ *              Mads Lundfold           mlundf20@student.aau.dk
+ *              Master Piyaboot         mprase20@student.aau.dk
+ * Study Group: A303
  * Supervisor: Kamal Shahid     Class: P1, Computer Science - Fall Semester
  *
- * Description: Here goes a description of this file.
+ * Description: This is the main file, from here the entire programme is run.
  */
 
 #ifndef ENERGY_H
--- Programme/main.c
+++ Programme/main.c
@@ -1,12 +1,12 @@
 /*
  * File: main.c                 Date completed: 16th of November, 2020
- * Programmers: Anders G.       E-mail: ageert20@student.aau.dk
- *              Christoffer T. J.       cjanss20@student.aau.dk
- *              Dennis B. L.            dbla19@student.aau.dk
- *              Jamie L. S. H.          jhamme20@student.aau.dk
- *              Loke W.                 lwalst20@student.aau.dk
- *              Mads L.                 mlundf20@student.aau.dk
- *              Master P. P.            mprase20@student.aau.dk
+ * Programmers: Anders Geert    E-mail: ageert20@student.aau.dk
+ *              Christoffer Jønsson     cjanss20@student.aau.dk
+ *              Dennis Larsen           dbla19@student.aau.dk
+ *              Jamie Hammer            jhamme20@student.aau.dk
+ *              Loke Walsted            lwalst20@student.aau.dk
+ *              Mads Lundfold           mlundf20@student.aau.dk
+ *              Master Piyaboot         mprase20@student.aau.dk
  * Study Group: A303
  * Supervisor: Kamal Shahid     Class: P1, Computer Science - Fall Semester
  *
