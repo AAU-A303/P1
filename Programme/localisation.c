6542499eab663b79a7b8793e856bfdaf3333acad
Jamie Hammer <jamie-hammer@hotmail.com>
2020-11-18 13:36:27 +0100

    Localisation
    Prepared the file for localisation manager

---
+++ Programme/localisation.c
@@ -0,0 +1,21 @@
+/*
+ ===============================================================================
+ * File: localisation.c             Date completed: 16th of November, 2020
+ ===============================================================================
+ * Programmers: Anders G.           E-mail: ageert20@student.aau.dk
+ *              Christoffer J.              cjanss20@student.aau.dk
+ *              Dennis L.                   dbla19@student.aau.dk
+ *              Jamie H.                    jhamme20@student.aau.dk
+ *              Loke W.                     lwalst20@student.aau.dk
+ *              Mads L.                     mlundf20@student.aau.dk
+ *              Master P.                   mprase20@student.aau.dk
+ ===============================================================================
+ * Study Group: A303
+ * Supervisor: Kamal Shahid         Class: P1, Computer Science - Fall Semester
+ ===============================================================================
+ * Description: This file manages our text strings in local languages.
+ ===============================================================================
+ */
+
+#include "energy.h"
+
